46d8447397ccfc4b54025eeba1bf48c16651701a
NaikChetana <chetananaik73@gmail.com>
2019-06-03 10:01:23 +0530

    Static files

--- src/app/directives/mark-down.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarkDownDirective } from './mark-down.directive';
-
-describe('MarkDownDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MarkDownDirective();
-    expect(directive).toBeTruthy();
-  });
-});
